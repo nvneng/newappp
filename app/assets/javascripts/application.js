de8deafe92681bd6907b220a3b455c0c1f632d58
naveen <naveenbollimpalli@gmail.com>
2015-05-12 04:10:29 +0530

    UI fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,128 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-angular.module("myapp", []).controller("MyController", function($scope, $http) {
-    $scope.myData = {};
-    $scope.myData.doClick = function(item, event) {
+$(document).ready(function(){
 
-        $scope.myData.fromServer = 'naveen';
 
+    $(".artist-view").hide();
+    $(".error-view").hide();
+    $(".load-view").hide();
+    $(".no-result-view").hide();
+
+    $(".backup_picture").error(function(){
+        $(this).attr('src', 'http://i61.tinypic.com/2mwdtsk.png');
+    });
+
+});
+
+angular.module("myApp", []).controller("MyController", function($scope, $http) {
+
+    $scope.historyData = {};
+    $scope.artistData = {};
+    $scope.moreData = {};
+
+    $scope.init = function () {
+
+        $scope.cpage = 0;
+        var response = $http.get("/history/get?cpage="+$scope.cpage);
+
+        response.success(function(data, status, headers, config) {
+
+            if( data.cpage === "nopageleft")
+            {
+                $("#more_button").hide();
+            }
+            else
+            {
+                $scope.historyData = data.history;
+                $scope.cpage = $scope.cpage+1;
+                $(".more_button").show();
+            }
+
+        });
+
+        response.error(function(data, status, headers, config) {
+
+        });
+    }
+
+
+
+
+
+
+    $scope.artistData.doClick = function(item, event) {
+
+
+        if(!$("#search-artist").val().match(/^\s*$/) ) {
+
+            $(".history-view").hide();
+            $(".load-view").show();
+            $scope.cpage = 0;
+            var response = $http.get("/search/artist?search_string=" + $("#search-artist").val());
+
+            response.success(function (data, status, headers, config) {
+
+                if (data.error || jQuery.isEmptyObject(data)) {
+                    //hide activate loading
+                    $(".load-view").hide();
+                    //show artist content
+                    $(".no-result-view").show();
+
+                }
+                else {
+                    $('#search-artist').val("");
+                    $scope.artistData.name = data.name;
+                    $scope.artistData.image = data.image;
+                    $scope.artistData.url = data.url;
+                    $scope.artistData.similar_artist = data.similar_artist;
+                    $scope.artistData.tags = data.tags;
+                    $scope.artistData.albums = data.albums;
+
+                    //hide activate loading
+                    $(".load-view").hide();
+                    //show artist content
+                    $(".artist-view").show();
+
+                }
+                $('#search-artist').val("");
+            });
+
+            response.error(function (data, status, headers, config) {
+                $('#search-artist').val("");
+                //hide activate loading
+                $(".load-view").hide();
+                //show artist content
+                $(".error-view").show();
+            });
+
+        }
+
+    }
+
+    $scope.moreData.g = function(item , event){
+
+        var response = $http.get("/history/get?cpage="+$scope.cpage);
+
+        response.success(function(data, status, headers, config) {
+            if( data.cpage === "nopageleft")
+            {
+                $("#more_button").hide();
+            }
+            else
+            {
+
+                $scope.historyData = $.merge($scope.historyData,data.history);// data.history;
+                $scope.cpage = $scope.cpage+1;
+                $("#more_button").show();
+
+            }
+
+        });
+
+        response.error(function(data, status, headers, config) {
+
+        });
     }
 
 
